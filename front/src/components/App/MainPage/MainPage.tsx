0a0fc6c67908e1c88c8ff8014f6b6b1e74a25093
Mikołaj Simon <mikolajsimon.nt@gmail.com>
2022-02-28 23:37:48 +0100

    [RDT-3] Header

--- front/src/components/App/MainPage/MainPage.tsx
+++ front/src/components/App/MainPage/MainPage.tsx
@@ -2,20 +2,23 @@ import Button from "../../UI/Button/Button"
 import ReactLogo from "../../UI/ReactLogo/ReactLogo"
 import RouteLink from "../../UI/RouteLink/RouteLink.styled"
 import { APP_PAGE } from "../App"
+import Header from "../../UI/Header/header"
+
 
 const MainPage = () => {
   return (
-    <div>
-      <header className="App-header">
+    <Header >
+      <header>
         <ReactLogo />
-        <RouteLink to={APP_PAGE.SALES}>
-          <Button title="Panel sprzedaży" />
-        </RouteLink>
-        <Button title="Panel administracyjny" />
-        <Button title="Statystyki" />
-        <Button title="Pomoc" />
       </header>
-    </div>
+
+      <RouteLink to={APP_PAGE.SALES}>
+        <Button title="Panel sprzedaży" />
+      </RouteLink>
+      <Button title="Panel administracyjny" />
+      <Button title="Statystyki" />
+      <Button title="Pomoc" />
+    </Header>
   )
 }
 
---
+++ front/src/components/UI/Header/header.tsx
@@ -0,0 +1,9 @@
+import { HeaderWrapper } from "./header.styled"
+import styled from "styled-components";
+
+const Header = styled(HeaderWrapper)`
+  color: inherit;
+  text-decoration: inherit;
+`;
+
+export default Header
