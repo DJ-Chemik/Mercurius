8e2a74f01116fa0eb3ba088b22fbd936d7b3113a
kostekd <dominikkostencki99@gmail.com>
2022-03-01 11:49:43 +0100

    [RDT-4] Commit Header with spinning logos

--- front/src/components/App/MainPage/MainPage.tsx
+++ front/src/components/App/MainPage/MainPage.tsx
@@ -1,11 +1,13 @@
 import Button from "../../UI/Button/Button"
 import ReactLogo from "../../UI/ReactLogo/ReactLogo"
 import RouteLink from "../../UI/RouteLink/RouteLink.styled"
+import Header from "../../UI/Header/Header.js";
 import { APP_PAGE } from "../App"
 
 const MainPage = () => {
   return (
     <div>
+      <Header></Header>
       <header className="App-header">
         <ReactLogo />
         <RouteLink to={APP_PAGE.SALES}>
