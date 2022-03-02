64ee49e3e17c32a424540f433f749448eacd349f
olmichal <olczefski@gmail.com>
2022-03-02 10:41:22 +0100

    [RDT-1] Change SalesArea background

--- front/src/components/App/SalesArea/SalesArea.tsx
+++ front/src/components/App/SalesArea/SalesArea.tsx
@@ -1,15 +1,27 @@
 import Button from "../../UI/Button/Button";
 import RouteLink from "../../UI/RouteLink/RouteLink.styled";
 import { APP_PAGE } from "../App";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: normal;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 
 const SalesArea = () => {
   return (
-    <div>
+    <Container>
       Sales Area
       <RouteLink to={APP_PAGE.HOME}>
         <Button title="Powrót do strony głównej" />
       </RouteLink>
-    </div>
+    </Container>
   )
 }
 
