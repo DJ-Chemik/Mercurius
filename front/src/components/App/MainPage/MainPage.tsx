70e7f48d4a85c108fa518e135836f53586412369
kostekd <dominikkostencki99@gmail.com>
2022-03-02 10:44:57 +0100

    [RDT-4] Refactor css to styled components

--- front/src/components/App/MainPage/MainPage.tsx
+++ front/src/components/App/MainPage/MainPage.tsx
@@ -1,7 +1,7 @@
 import Button from "../../UI/Button/Button"
 import ReactLogo from "../../UI/ReactLogo/ReactLogo"
 import RouteLink from "../../UI/RouteLink/RouteLink.styled"
-import Header from "../../UI/Header/Header.js";
+import Header from "../../UI/Header/Header";
 import { APP_PAGE } from "../App"
 
 const MainPage = () => {
---
+++ front/src/components/UI/Header/Header.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from '../../../assets/logo.svg';
+import { MainPanel, PanelCenter, PanelElement, PanelImageLeft, PanelImageRight } from "./Header.styled";
+import './Header.styled.ts';
+
+
+const Header = () => {
+  return (
+    <MainPanel>
+      
+      <PanelElement>
+        <PanelCenter>
+        <PanelImageLeft src={logo}/>
+        </PanelCenter>
+      </PanelElement>
+
+      <PanelElement>
+        <PanelCenter>
+            <h1>React Project</h1>
+          </PanelCenter>
+      </PanelElement>
+
+      <PanelElement>
+        <PanelCenter>
+          <PanelImageRight src={logo}/>
+        </PanelCenter>
+      </PanelElement>
+
+    </MainPanel>
+  );
+}
+
+
+export default Header;
