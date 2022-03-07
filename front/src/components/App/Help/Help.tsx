260806d1bb5a7eb53ffe1b5902d70f40b484fdd2
kostekd <dominikkostencki99@gmail.com>
2022-03-07 13:16:43 +0100

    [RDT-15] Structure data passed in props

--- front/src/components/App/Help/Help.tsx
+++ front/src/components/App/Help/Help.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {v4 as uuid_v4} from "uuid";
 import Button from "../../UI/Button/Button";
 import Header from "../../UI/Header/Header";
@@ -7,23 +7,40 @@ import Tabs from "../../UI/Tabs/Tabs";
 import { APP_PAGE } from "../App";
 import { HelpStyled } from "./Help.styled";
 
-
-
 //Just for testing
+export interface PackageData {
+    id: string;
+    name: string;
+    content: string;
+};
 
 const Help = () => {
+    //Variables
+    const LOCAL_STORAGE_KEY = 'elements';
+
     //States
-    const [elements, setElements] = useState(["Allegro", "Olx", "Amazon", "e-Bay"]);
+    const [elements, setElements] = useState<PackageData[]>([]);
 
     //parameter just for testing
     const addElementHandler = (name="Test") =>{
-        setElements([...elements, name]);
+
+        const request = {
+            id: uuid_v4(),
+            name: "Allegro",
+            content: "Ale fajnie",
+        }
+        console.log(request);
+        setElements([...elements, request]);
     };
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(elements))
+    }, [elements]);
 
     return (
         <HelpStyled>
           <Header/>
-            <Tabs names={elements} addElementHandler = {addElementHandler}/>
+            <Tabs packageData={elements} addElementHandler = {addElementHandler}/>
             <RouteLink to={APP_PAGE.HOME}>
                 <Button title="Powrót do strony głównej" />
             </RouteLink>
--- front/src/components/UI/Tabs/Tabs.tsx
+++ front/src/components/UI/Tabs/Tabs.tsx
@@ -1,24 +1,26 @@
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import { PackageData } from '../../App/Help/Help';
 import { AddStyled, ContentStyled, SingleTab, TabsStyled, TitleStyled } from "./Tabs.styled";
 import './Tabs.styled.ts';
 
-const Tabs = (props: { names: string[], addElementHandler: Function }) => {
+const Tabs = (props: { packageData: PackageData[], addElementHandler: Function }) => {
 
-  const addContentHandler = (elements: string[]): PropTypes.ReactNodeArray => {
+  const addContentHandler = (elements: PackageData[]): PropTypes.ReactNodeArray => {
     const elementHTMLarray = [];
 
     for (let i = 0; i < elements.length; i++) {
+      let {id, name, content} = elements[i];
 
       elementHTMLarray.push(
+        <Link to={{pathname: '/edit/{:id}', state: {tab : props.packageData[i]}}}>
         <SingleTab>
-          <TitleStyled>{elements[i]}</TitleStyled>
-          <ContentStyled> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-            The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-            Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-            Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-            
+          <TitleStyled>{name}</TitleStyled>
+          <ContentStyled> 
+            {content}
           </ContentStyled>
         </SingleTab>
+        </Link>
       );
     }
     return elementHTMLarray;
@@ -27,7 +29,7 @@ const Tabs = (props: { names: string[], addElementHandler: Function }) => {
 
   return (
     <TabsStyled >
-      {addContentHandler(props.names)}
+      {addContentHandler(props.packageData)}
       <AddStyled onClick={() => props.addElementHandler()} />
     </TabsStyled>
   )
