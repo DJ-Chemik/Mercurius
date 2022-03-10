70a3b24f3ca8e47135a99fb6b1b68fabcf84c0da
olmichal <olczefski@gmail.com>
2022-03-10 16:41:32 +0100

    [RDT-13] Remove line

--- front/src/components/UI/Button/Button.tsx
+++ front/src/components/UI/Button/Button.tsx
@@ -3,12 +3,11 @@ import { ButtonWrapper } from "./Button.styled"
 interface ButtonProps {
   title: string;
   onClick?: () => void;
-  type?: string;
 }
 
 const Button = (props: ButtonProps) => {
   return (
-    <ButtonWrapper onClick={props.onClick} >
+    <ButtonWrapper onClick={props.onClick}>
       {props.title}
     </ButtonWrapper>
   )
