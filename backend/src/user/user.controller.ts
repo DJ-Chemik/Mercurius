5787e8488263e6729f271fa91d1bf3ebb039e94d
kostekd <dominikkostencki99@gmail.com>
2022-03-19 12:40:14 +0100

    [RDT-22] Fix remarks

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Delete, Get, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Delete(':id')
-  remove(@Param('id') id: Number): string {
+  remove(@Param('id') id: number): string {
     return `Delete the user #${id}`;
   }
 }
