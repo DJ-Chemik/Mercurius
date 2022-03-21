92b91069bacf908f94d2b1589db92ef1ea71b737
tomaszStr1020 <tomasz.strzeszewski@student.put.poznan.pl>
2022-03-21 13:15:36 +0100

    Code cleaning

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,9 @@ import {UserModule} from "./user/user.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const usr = await NestFactory.create(UserModule);
   const prismaService: PrismaService = app.get(PrismaService);
-  const prismaService2: PrismaService = usr.get(PrismaService);
   await prismaService.enableShutdownHooks(app)
-  await prismaService2.enableShutdownHooks(usr)
   await app.listen(4000);
-  await usr.listen(4001);
+
 }
 bootstrap();
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import {UserService} from "./user.service";
 import {Controller, Get} from "@nestjs/common";
 
-@Controller()
+@Controller('/newuser')
 export class UserController{
     constructor(private readonly userService: UserService){}
 
