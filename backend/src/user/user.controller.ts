a705c810ec683a82ab0f565e236d2faf0d3da3a6
LenaPtak <lenkaptak@gmail.com>
2022-04-03 14:25:54 +0200

    [RDT-20] Trying to fetch a user from the database

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,21 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Redirect } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller('/users')
+@Controller('users')
 export class UserController {
-constructor(private readonly userService: UserService) {
-}
-    @Get(':id')
-    getUserById(@Param('id') id: string)
-    {
-        const data = {id: "id"}
-        return id;
-    }
+    constructor(private readonly userService: UserService) {}
+        @Get(':id')
+        getUserById(@Param('id') id: number)
+        {
+            const userId = {id: "id"}
+            //this.userService.user(userId)
+            return `Get user by id: #${id}`;
+        }
 
-    @Get('/byMail:mail')
-    getUserByMail(@Param('mail') mail: string)
-    {
-        const data = {email: "email"}
-        return mail;
-    }
+        @Get('/mail:mail')
+        @Redirect('https://docs.nestjs.com')
+        getUserByMail(@Param('mail') mail: string)
+        {
+            const userMail = {email: "email"}
+            //this.userService.updateUser(userMail)
+            return `Get user by mail: #${mail}`;
+        }
 }
