78004ed3f2cbb6fd786b13cd8301e7274367d0d1
Tomasz Strzeszewski <tomasz.strzeszewski@student.put.poznan.pl>
2022-04-11 22:53:34 +0200

    Cleaned and working version

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,13 +1,14 @@
 import {UserService} from "./user.service";
-import {Controller, Post} from "@nestjs/common";
+import {Body, Controller, Post} from "@nestjs/common";
+import {Prisma} from "@prisma/client";
 
-@Controller('createuser')
+@Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) {
     }
-    @Post()
-    createNewUser(email: string, name: string) {
-        const data = {email: email, login: name}
-        this.userService.createUser(data)
+    @Post('create-user')
+    createNewUser(@Body() newUserData:  Prisma.UserCreateInput) {
+        this.userService.createUser(newUserData)
+        return "Pomyślnie dodano użytkownika o adresie email: "+ newUserData.email + " oraz nazwie: "+newUserData.login
     }
 }
